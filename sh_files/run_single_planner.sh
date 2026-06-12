b550cf1636eb922cf98e72212d75f3f58741f8f0
rqydamowang <841231447@qq.com>
2026-06-12 11:10:39 +0800

    update bridge_tcp_drone.launch

--- sh_files/run_single_planner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-echo 'nv' | sudo -S chmod 777 /dev/tty* & sleep 1;
-export DRONE_ID=0;
-source devel/setup.zsh;
-roslaunch diff_planner run_exp_single_lio.launch & sleep 3;
-roslaunch diff_planner exp_rviz.launch & sleep 1;
-wait;
